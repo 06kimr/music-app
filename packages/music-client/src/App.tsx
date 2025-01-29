24fa8a82cb14e20445c92fc0d37d4b6c8c47f12b
rickykim-99 <youngs.kim@creadto.com>
2025-01-29 23:55:05 +0900

    [FEAT] msw added

--- packages/music-client/src/App.tsx
+++ packages/music-client/src/App.tsx
@@ -1,11 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useGetSongs from "./hooks/useGetSongs";
+
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <div className="flex w-full text-blue-500 bg-red-500">
-    asdasd
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <TemComponent />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+function TemComponent() {
+  const { data } = useGetSongs();
+  return (
+    <div className="flex w-full text-blue-500 bg-red-500">
+      {data ? JSON.stringify(data) : "loading..."}
+    </div>
+  );
+}
+
+export default App;
