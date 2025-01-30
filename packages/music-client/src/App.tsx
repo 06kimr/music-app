3982ea2841f74a39110072bad8fcd7961b866028
rickykim-99 <youngs.kim@creadto.com>
2025-01-30 18:29:24 +0900

    [FEAT] Section Presentational Layer

--- packages/music-client/src/App.tsx
+++ packages/music-client/src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import useGetSongs from "./hooks/useGetSongs";
+import { useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import useGetSongs from "./hooks/useGetSongs";
 import ErrorFallback from "./presentationals/common/ErrorFallback";
 import RootLayout from "./presentationals/common/RootLayout";
-import PlayerWrapper from "./presentationals/player/PlayerWrapper";
-import SongCard from "./presentationals/common/SongCard";
 import SliderPanel from "./presentationals/common/SliderPanel";
-import { useState } from "react";
+import SectionPanel from "./presentationals/home/SectionPanel";
+import PlayerWrapper from "./presentationals/player/PlayerWrapper";
 
 const queryClient = new QueryClient();
 
@@ -24,7 +24,7 @@ function App() {
         </ErrorBoundary>
         <SliderPanel open={open} onClose={handleClose}>
           <div className="w-[300px]">
-              <h1>재생목록</h1>
+            <h1>재생목록</h1>
           </div>
         </SliderPanel>
       </RootLayout>
@@ -36,20 +36,7 @@ function App() {
 function TemComponent() {
   const { data } = useGetSongs();
   return (
-    <div className="flex gap-x-20">
-      {data?.map((song) => (
-        <SongCard key={song.id} variant="vertical">
-          <SongCard.Image
-            src={"https://placehold.co/150"}
-            alt={song.title}
-          />
-          <SongCard.Content>
-            <SongCard.Title>{song.title}</SongCard.Title>
-            <SongCard.Description>{song.artist}</SongCard.Description>
-          </SongCard.Content>
-        </SongCard>
-      ))}
-    </div>
+   <SectionPanel  songs={data ?? []} moreLink="/" title="패캠을 위한 믹스 & 추천"/>
   );
 }
 
---
+++ packages/music-client/src/presentationals/home/SectionPanel.tsx
@@ -0,0 +1,37 @@
+import Section from "../common/Section";
+import SongCard from "../common/SongCard";
+
+interface Props {
+  songs: Song[];
+  title: string;
+  moreLink: string;
+}
+
+export default function SectionPanel({ songs, title, moreLink }: Props) {
+  return (
+    <Section>
+      <Section.Title className="flex justify-between">
+        <span>{title}</span>
+        <a className="font-medium text-gray300 text-16" href={moreLink}>
+          All
+        </a>
+      </Section.Title>
+      <Section.Content>
+        <div className="flex gap-x-18">
+          {songs?.map((song) => (
+            <SongCard key={song.id} variant="vertical" className="shrink-0">
+              <SongCard.Image
+                src={"https://placehold.co/150"}
+                alt={song.title}
+              />
+              <SongCard.Content>
+                <SongCard.Title>{song.title}</SongCard.Title>
+                <SongCard.Description>{song.artist}</SongCard.Description>
+              </SongCard.Content>
+            </SongCard>
+          ))}
+        </div>
+      </Section.Content>
+    </Section>
+  );
+}
